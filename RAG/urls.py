5f1e0f07787173bb41b965366e15cc0cc28b1c54
htk96 <49614453+htk96@users.noreply.github.com>
2024-03-20 23:51:09 +0900

    요약 버전1 완료

--- RAG/urls.py
+++ RAG/urls.py
@@ -8,5 +8,5 @@ from .views import (
 urlpatterns = [
     path('', views.index, name='index'),
     path('crawl-news/', crawl_news_view, name='crawl-news'),
-    
+    path('summarize/<int:news_id>/', views.summarize_text, name='summarize_text'),
 ]
--- RAG/views.py
+++ RAG/views.py
@@ -1,13 +1,18 @@
 from django.urls import reverse
-from django.http import HttpResponseRedirect
+from django.http import JsonResponse
 from django.contrib import messages
 from datetime import datetime
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 import requests
 from bs4 import BeautifulSoup
 from django.utils import timezone
 from .models import News
+
+from .models import News, Summary
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+import nltk
+nltk.download('punkt')
 
 def index(request):
     return render(request, 'index.html')
@@ -67,3 +72,27 @@ def crawl_news_view(request):
     
     return render(request, 'index.html', {'news_list': news_list})
 
+
+tokenizer = AutoTokenizer.from_pretrained('eenzeenee/t5-base-korean-summarization')
+model = AutoModelForSeq2SeqLM.from_pretrained('eenzeenee/t5-base-korean-summarization')
+
+def summarize_text(request, news_id):
+    news_article = get_object_or_404(News, pk=news_id)
+    text_to_summarize = news_article.content  # 가정: News 모델에 'text' 필드가 있다고 가정합니다.
+    
+    prefix = "summarize: "
+    inputs = tokenizer(prefix + text_to_summarize, return_tensors="pt", padding=True, truncation=True, max_length=512)
+    summary_outputs = model.generate(**inputs, num_beams=4, max_length=200, early_stopping=True)
+    summary_text = tokenizer.decode(summary_outputs[0], skip_special_tokens=True)
+
+    # 요약을 데이터베이스에 저장합니다.
+    summary, created = Summary.objects.get_or_create(
+        news=news_article,
+        defaults={'summary_text': summary_text}
+    )
+    if not created:
+        summary.summary_text = summary_text
+        summary.save()
+
+    # 요약된 텍스트와 함께 페이지를 렌더링합니다.
+    return JsonResponse({'summary': summary.summary_text})
